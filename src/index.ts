2f3be9f03160e63b3ba349cb18e2baffc4a7b1ae
zicoz18 <zicoz18@ku.edu.tr>
2023-04-16 00:22:46 +0300

    Add functionality to bootstrap from empty db and syncronize missing blocks

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,44 @@
 import initMongoDB from './init/db'
-import { initWSProvider } from './init/provider'
-import { ITransaction } from './interfaces'
-import { formatTransactionsData, formatBlockData } from './utils/formatData'
+import { initRPCProvider } from './init/provider'
 import { getCollections } from './utils/getCollections'
-
-const wsProvider = initWSProvider('wss://api.avax-test.network/ext/bc/C/ws')
+import { config } from 'dotenv'
+import { processBlock } from './utils/processBlock'
+import { findMissingBlocks } from './utils/findMissingBlocks'
+config()
 
 const main = async () => {
+  // Init rpc provider
+  if (!process.env.RPC_URL) {
+    throw Error('.env should inlcude RPC_URL')
+  }
+  const rpcProvider = initRPCProvider(process.env.RPC_URL)
   // Connect to db
   const db = await initMongoDB()
   // Get collections: block and transaction
   const collections = getCollections(db)
 
+  const blockCount = await collections.block.countDocuments()
+  const recentBlock = await rpcProvider.getBlockNumber()
+
+  // If there is no block data inside the collection it means that we are indexing for the first time
+  // Therefore, process block from index 0 to current blockNumber
+  if (blockCount === 0) {
+    const startBlock = 0
+    for (let i = startBlock; i < recentBlock; i++) {
+      processBlock({ blockNumber: i, rpcProvider, collections })
+    }
+    // If there is blocks missing
+  } else if (blockCount !== recentBlock + 1) {
+    // Find missing block numbers and process them
+    const missingBlockNumbers = await findMissingBlocks({ collections })
+    for (let i = 0; i < missingBlockNumbers.length; i++) {
+      processBlock({ blockNumber: missingBlockNumbers[i], rpcProvider, collections })
+    }
+  }
+
   // Subscribe to new blocks
-  wsProvider.on('block', async (newBlockNumber) => {
-    // Get block with transactions, given block number
-    const blockWithTransactions = await wsProvider.getBlockWithTransactions(newBlockNumber)
-    const { transactions, ...block } = blockWithTransactions
-
-    // Format block and transactions in a way that will be useful for us
-    const formattedBlock = formatBlockData(block)
-    const formattedTransactions = formatTransactionsData(transactions as unknown as ITransaction[])
-
-    // Insert block and transaction data to our db
-    await collections.block.insertOne(formattedBlock)
-    await collections.transaction.insertMany(formattedTransactions)
-    console.log(`Processed block ${newBlockNumber}`)
+  rpcProvider.on('block', async (blockNumber) => {
+    processBlock({ blockNumber, rpcProvider, collections })
   })
 }
 main()
